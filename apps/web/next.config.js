8206b587554e364343d557261295b47774f8bcc6
CubeStorm <cubestorm72@gmail.com>
2022-11-09 21:56:00 +0100

    config/add storybook's config for svgs

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
 	experimental: {
 		appDir: true,
 	},
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			use: ['@svgr/webpack', 'url-loader'],
+		});
+
+		return config;
+	},
 };
 
 module.exports = nextConfig;

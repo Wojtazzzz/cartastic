1f1ebc33b36ac86130b98d850fd99612aad417fc
CubeStorm <cubestorm72@gmail.com>
2022-11-16 16:41:12 +0100

    feat/add and configure cors

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -5,6 +5,9 @@ export type AppOptions = {} & Partial<AutoloadPluginOptions>;
 const options: AppOptions = {};
 
 const app: FastifyPluginAsync<AppOptions> = async (fastify, opts): Promise<void> => {
+	void fastify.register(import('@fastify/cors'), {
+		origin: 'http://localhost:3000',
+	});
 	void fastify.register(import('@fastify/swagger'));
 	void fastify.register(import('@fastify/swagger-ui'), { routePrefix: '/docs' });
 

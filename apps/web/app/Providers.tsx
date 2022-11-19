b52a804911b41c7759e8209aa5267e3252eab2ef
CubeStorm <cubestorm72@gmail.com>
2022-11-19 21:38:51 +0100

    refactor/add function for creating query clients

--- apps/web/app/Providers.tsx
+++ apps/web/app/Providers.tsx
@@ -1,13 +1,14 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import type { ReactNode } from 'react';
+import { createQueryClient } from 'utils/createQueryClient';
 
 type ProvidersProps = {
 	children: ReactNode;
 };
 
-const queryClient = new QueryClient();
+const queryClient = createQueryClient();
 
 export const Providers = ({ children }: ProvidersProps) => {
 	return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
--- apps/web/utils/renderWithProviders.tsx
+++ apps/web/utils/renderWithProviders.tsx
@@ -1,9 +1,10 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { render } from '@testing-library/react';
 import type { ReactElement } from 'react';
+import { createQueryClient } from './createQueryClient';
 
 export const renderWithProviders = (component: ReactElement) => {
-	const queryClient = new QueryClient();
+	const queryClient = createQueryClient();
 
 	render(<QueryClientProvider client={queryClient}>{component}</QueryClientProvider>);
 };

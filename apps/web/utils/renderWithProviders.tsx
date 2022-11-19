10c254dda9f9b30886a96e6f2135fd83cb6576c4
CubeStorm <cubestorm72@gmail.com>
2022-11-19 18:42:30 +0100

    refactor/add QueryClientProvider to render function

--- apps/web/utils/renderWithProviders.tsx
+++ apps/web/utils/renderWithProviders.tsx
@@ -1,6 +1,9 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render } from '@testing-library/react';
 import type { ReactElement } from 'react';
 
 export const renderWithProviders = (component: ReactElement) => {
-	render(component);
+	const queryClient = new QueryClient();
+
+	render(<QueryClientProvider client={queryClient}>{component}</QueryClientProvider>);
 };

3c56951c56ab37c2497f64728550ecb4e096d8f9
CubeStorm <cubestorm72@gmail.com>
2022-12-06 11:25:01 +0100

    feat/add alert after copy discord

--- apps/web/components/molecules/footer/contactSection/copyDiscordToClipboard.ts
+++ apps/web/components/molecules/footer/contactSection/copyDiscordToClipboard.ts
@@ -3,6 +3,8 @@ const DISCORD = 'Wojtazz#8680';
 export const copyDiscordToClipboard = () => {
 	const copy = () => {
 		navigator.clipboard.writeText(DISCORD);
+
+		alert('Discord name copied!');
 	};
 
 	return copy;

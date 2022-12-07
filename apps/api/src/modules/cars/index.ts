0af734f60a104e5942c0426b7b3cd205449ab166
CubeStorm <cubestorm72@gmail.com>
2022-12-07 19:03:11 +0100

    feat/add endpoint for latests offers

--- apps/api/src/modules/cars/index.ts
+++ apps/api/src/modules/cars/index.ts
@@ -1,5 +1,8 @@
-import type { FastifyPluginCallbackTypebox } from '@fastify/type-provider-typebox';
-import { getCarsCountSchema } from './cars.schema';
+import type {
+	FastifyPluginCallbackTypebox,
+	TypeBoxTypeProvider,
+} from '@fastify/type-provider-typebox';
+import { getCarsCountSchema, getLatestCarsSchema } from './cars.schema';
 import { getCarsCount } from './cars.service';
 
 const carsModule: FastifyPluginCallbackTypebox = (fastify, _options, done) => {
@@ -7,6 +10,39 @@ const carsModule: FastifyPluginCallbackTypebox = (fastify, _options, done) => {
 		const prisma = fastify.prisma;
 
 		return getCarsCount(prisma);
+	});
+
+	fastify.withTypeProvider<TypeBoxTypeProvider>().route({
+		url: '/latest',
+		method: 'GET',
+		schema: getLatestCarsSchema,
+		async handler(request, reply) {
+			const cars = await fastify.prisma.car.findMany({
+				include: {
+					brand: {
+						select: {
+							id: true,
+							name: true,
+						},
+					},
+					model: {
+						select: {
+							id: true,
+							name: true,
+						},
+					},
+				},
+			});
+
+			const data = cars.map((car) => ({
+				...car,
+				images: Array.isArray(car.images)
+					? car.images?.map((img) => JSON.stringify(img))
+					: [],
+			}));
+
+			return data;
+		},
 	});
 
 	done();
